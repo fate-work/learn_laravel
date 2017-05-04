d283a4a01bd6c2f0c6d9094388cc2f6f12c51ef8
fate <darcnus@yandex.ru>
2017-05-04 13:19:06 +0300

    Learn Eloquent Many To Many

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -6,12 +6,16 @@ use App\home;
 use App\usersmobile;
 use Illuminate\Http\Request;
 use App\user;
+use App\usersrole;
+use App\role;
 
 class homeController extends Controller
 {
     public function welcome(){
-        $user=usersmobile::find(1)->mobile;
-        return view("welcome",compact('user'));
+        $role=usersrole::find(1)->roles;
+        $user=role::find(2)->usersroles;
+        return $role;
+        //return view("welcome",compact('user'));
     }
     public function about(){
         return view("about");
