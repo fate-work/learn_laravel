b30c81a85092f25e7a09a1b6b86e45dfbc7ca5bc
fate <darcnus@yandex.ru>
2017-05-23 17:43:55 +0300

    Learn about Mutators

--- app/todo.php
+++ app/todo.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class todo extends Model
 {
-    public function getBodyAttribute($value)
+  /*  public function getBodyAttribute($value)
     {
         return ucfirst($value);
+    }*/
+    public function setBodyAttribute($value)
+    {
+        return $this->attributes['body']=ucfirst($value);
     }
 }
